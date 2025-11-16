9a79b9ed4b0e23375ced464a088bff5359eca279
kevinrhu <kevinrouhu@gmail.com>
2025-11-16 16:27:33 -0500

    added categorical preprocessing

---
+++ preprocess.py
@@ -0,0 +1,25 @@
+
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+
+TRAINING_DATA = "training_data_clean.csv"
+
+def prep_data(data=TRAINING_DATA):
+    df = pd.read_csv(data)
+    # rename columns 
+    df.columns = [
+        'id', 'best_tasks_free', 'acad_tasks_rating', 'best_tasks_select', 'subopt_freq_select',  'subopt_tasks_select', 'subopt_tasks_free', 
+        'evidence_freq_select', 'verify_freq_select', 'verify_method_free']
+    
+    students = data['id'].unique()
+    train_df, test_df = train_test_split(students, test_size=0.3, random_state=42)
+
+def impute_data():
+    """replacing missing or inconsistent values in a dataset with estimated values 
+    
+    numerical data: replace with median, scaled with standard scaler
+    categorical data: replace with mode 
+    text data: replace with empty string
+    """
+    # TODO: can use KNN to find the imputation point instead, in-person imputation
+    pass 
