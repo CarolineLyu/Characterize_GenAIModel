4ba03d38ff0d236dac6ee559e32cd5e507965e7f
CarolineLyu <168619926+CarolineLyu@users.noreply.github.com>
2025-11-16 13:27:21 -0500

    full embed text loop uncommented

--- preprocess.py
+++ preprocess.py
@@ -1,15 +1,28 @@
 
 import pandas as pd 
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 TRAINING_DATA = "training_data_clean.csv"
 
 def prep_data(data=TRAINING_DATA):
     df = pd.read_csv(data)
-    # rename columns 
+    print(df.dtypes)
+    
+    # rename
     df.columns = [
-        'id', 'best_tasks_free', 'acad_tasks_rating', 'best_tasks_select', 'subopt_freq_select',  'subopt_tasks_select', 'subopt_tasks_free', 
-        'evidence_freq_select', 'verify_freq_select', 'verify_method_free']
+        'id', 
+        'best_tasks_free', 
+        'acad_tasks_rating', 
+        'best_tasks_select', 
+        'subopt_freq_select',  
+        'subopt_tasks_select', 
+        'subopt_tasks_free', 
+        'evidence_freq_select', 
+        'verify_freq_select', 
+        'verify_method_free', 
+        'target'
+        ]
     
     students = data['id'].unique()
     train_df, test_df = train_test_split(students, test_size=0.3, random_state=42)
